5d21383a7cc23f80e3739bd1fccbc0a60b99faa1
Rodolphe Moutte <rodolphe.moutte@gmail.com>
2017-02-26 23:12:56 +0100

    Added synchronization base command

---
+++ app/Console/Commands/SynchronizeMedias.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\SynchronizeMedia;
+use App\Models\Setting;
+use App\Models\Song;
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Support\Collection;
+
+class SynchronizeMedias extends Command
+{
+    use DispatchesJobs;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'kms:sync';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sync songs found in configured directory against the database.';
+
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    /**
+     * Create a new command instance.
+     *
+     * @param Filesystem $filesystem
+     * @throws \Exception
+     */
+    public function __construct(Filesystem $filesystem)
+    {
+        parent::__construct();
+
+        $this->filesystem = $filesystem;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $mediasToSync = $this->getMediasToSync();
+
+        foreach ($mediasToSync as $mediaToSync) {
+            $this->dispatchNow(new SynchronizeMedia($mediaToSync));
+        }
+    }
+
+    /**
+     * @return Collection
+     */
+    private function getMediasToSync()
+    {
+        $mediasFromFileSystem = $this->getMediasFromFilesystem();
+        $alreadySyncedMedias = $this->getAlreadySyncedMedias();
+
+        return $mediasFromFileSystem->diffKeys($alreadySyncedMedias);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    private function getAlreadySyncedMedias()
+    {
+        return Song::all()->keyBy('id');
+    }
+
+    /**
+     * @return Collection
+     * @throws \Exception
+     */
+    private function getMediasFromFilesystem()
+    {
+        if (!$this->filesystem->isDirectory(Setting::get('media_path'))) {
+            throw new \Exception();
+        }
+
+        $path = Setting::get('media_path');
+        $medias = $this->filesystem->allFiles($path);
+
+        return Collection::make($medias)
+            ->keyBy(function ($file) {
+                return sha1_file($file);
+            });
+    }
+}
