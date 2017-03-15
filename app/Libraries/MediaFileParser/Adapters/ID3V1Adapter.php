783ea79c896d680d0bb0e4ac1174b61242db963e
Moutte Rodolphe <rodolphe.moutte@gmail.com>
2017-03-15 10:02:18 +0100

    Added missing Id3V1 Adapter

---
+++ app/Libraries/MediaFileParser/Adapters/ID3V1Adapter.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Libraries\MediaFileParser\Adapters;
+
+/**
+ * Class ID3V1Adapter
+ * @package App\Libraries\MediaFileParser\Adapters
+ */
+class ID3V1Adapter extends BaseAdapter
+{
+    /**
+     * @return int|null
+     */
+    public function guessTrackNumber()
+    {
+        // TODO: Implement guessTrackNumber() method.
+    }
+
+    /**
+     * @return string|null
+     */
+    public function guessTitle()
+    {
+        // TODO: Implement guessTitle() method.
+    }
+
+    /**
+     * @return string|null
+     */
+    public function guessArtistName()
+    {
+        // TODO: Implement guessArtistName() method.
+    }
+
+    /**
+     * @return string|null
+     */
+    public function guessGroupName()
+    {
+        // TODO: Implement guessGroupName() method.
+    }
+
+    /**
+     * @return string|null
+     */
+    public function guessGenreName()
+    {
+        // TODO: Implement guessGenreName() method.
+    }
+
+    /**
+     * @return int|null
+     */
+    public function guessYear()
+    {
+        // TODO: Implement guessYear() method.
+    }
+}
