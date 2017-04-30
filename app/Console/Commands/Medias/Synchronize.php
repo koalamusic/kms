840d27566a237ec382895b5555d775f1341bdb5c
Moutte Rodolphe <rodolphe.moutte@gmail.com>
2017-04-30 14:45:57 +0200

    :/

--- app/Console/Commands/Medias/Synchronize.php
+++ app/Console/Commands/Medias/Synchronize.php
@@ -111,7 +111,7 @@ class Synchronize extends Command
      */
     protected function getFileHash(SplFileInfo $file)
     {
-        $process = new Process("sha1sum '{$file->getFilename()}'");
+        $process = new Process("sha1sum '{$file->getRealPath()}'");
         $process->run();
 
         if (! $process->isSuccessful()) {
