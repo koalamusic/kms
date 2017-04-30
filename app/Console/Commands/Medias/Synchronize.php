eb1421c66428a6e6286aa86363c2449c30377278
Moutte Rodolphe <rodolphe.moutte@gmail.com>
2017-04-30 13:46:16 +0200

    Another test

--- app/Console/Commands/Medias/Synchronize.php
+++ app/Console/Commands/Medias/Synchronize.php
@@ -94,7 +94,7 @@ class Synchronize extends Command
         $bar = $this->output->createProgressBar(count($medias));
 
         $medias = Collection::make($medias)->keyBy(function (SplFileInfo $file) {
-            return hash_file('crc32b', $file);
+            return crc32($file);
         })->each(function () use (&$bar) {
             $bar->advance();
         });
