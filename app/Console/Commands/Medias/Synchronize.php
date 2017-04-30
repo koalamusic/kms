6d8998ede9116f22c24e353436c7f142d2626262
Moutte Rodolphe <rodolphe.moutte@gmail.com>
2017-04-30 13:48:22 +0200

    Another test

--- app/Console/Commands/Medias/Synchronize.php
+++ app/Console/Commands/Medias/Synchronize.php
@@ -91,15 +91,9 @@ class Synchronize extends Command
 
         $this->info('Building collection ...');
 
-        $bar = $this->output->createProgressBar(count($medias));
-
         $medias = Collection::make($medias)->keyBy(function (SplFileInfo $file) {
             return crc32($file);
-        })->each(function () use (&$bar) {
-            $bar->advance();
         });
-
-        $bar->finish();
 
         $this->info('End building collection ...');
 
