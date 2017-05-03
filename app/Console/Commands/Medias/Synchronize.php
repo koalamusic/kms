c70ee9f61ed390c2c7ec052349f4825cb61388e0
Rodolphe Moutte <r.moutte@web-6.fr>
2017-05-03 13:52:08 +0200

    Dispatching async jobs

--- app/Console/Commands/Medias/Synchronize.php
+++ app/Console/Commands/Medias/Synchronize.php
@@ -62,7 +62,7 @@ class Synchronize extends Command
         $bar = $this->output->createProgressBar($mediasToSync->count());
 
         foreach ($mediasToSync as $mediaToSync) {
-            $this->dispatchNow(new SynchronizeMedia($mediaToSync));
+            $this->dispatch(new SynchronizeMedia($mediaToSync));
 
             $bar->advance();
         }
